8f75b8248b521b64fce5e57e10ec0310058886cb
Jeff Mcmahan <jeff@Jeffs-MacBook-Pro.local>
2018-10-30 18:14:22 -0400

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# MinBuild
+
+Sometimes using tools like Webpack, Browserify, or whatever else, is sort of a bummer. Especially for smaller projects, or for code that you won't be keeping up with much (who wants fix a 4 year-old build configuation?). Yet having the freedom to split frontend code into files is really nice. So here's a tool that's simple to the point of stupidity, which lets you create large structured codebases.
+
+All of the module management is done via a single dependency object, accessed from the `include` and `declare` identifiers (declare these as globals in your linter).
+
+```js
+const foo = include.foo
+const {bar, baz} = include.anotherThing
+
+declare.myModule = function () {
+	// ...
+}
+```
+
+Don't use `declare` in your project's main file.
+
+```shell
+cd ~/myproject && minbuild
+```
+
+This will create an output file called `myproject.compiled.js`.
---
+++ index.js
@@ -0,0 +1,108 @@
+'use strict'
+
+const {join, basename} = require('path')
+const {readdirSync, readFileSync, statSync, writeFileSync} = require('fs') 
+const files = []
+
+// Recursively get all the files in the directory.
+function readdir(dirPath) {
+	readdirSync(dirPath).forEach(relPath => {
+		const contentAbsPath = join(dirPath, relPath)
+		if (statSync(contentAbsPath).isFile()) {
+			const content = readFileSync(contentAbsPath, 'utf8').trim()
+			if (!content.includes('/*__COMP*/')) {
+				files.push({content, fpath: contentAbsPath})
+			}
+		} else {
+			readdir(contentAbsPath)
+		}
+	})
+	return files
+}
+
+// Find the name from an epression of the form: "declare.theName".
+function getName(file) {
+	const tail = file.content.split('declare.')[1] || 'anonymous/main'
+	const spacePos = tail.search(/[\s;]/)
+	return tail.slice(0, ~spacePos ? spacePos : tail.length)
+}
+
+function wrap(file) {
+	const name = getName(file) || 'main'
+	const comment = '// ' + name + ' '
+	return (
+		';void function(){\n' + comment + '\n' +
+		`\n${file.content}\n\n` + ''.padStart(40, '\t') + '}()'
+	)
+}
+
+function removeNonIncluded(files) {
+	return files.filter(file => { // Drop files that are not needed.
+		const name = getName(file)
+		return (
+			name == 'anonymous/main' ||
+			files.some(otherFile => otherFile.content.includes('include.' + name))
+		)
+	})
+}
+
+function max1Declare(files) {
+	files.forEach(file => { // 1 "declare" permitted per file.
+		if (file.content.split('declare.').length > 2) {
+			throw new Error(`Multiple declare statements in: ...${file.fpath.slice(-20)}`)
+		}
+	})
+}
+
+function organize(files) {
+	max1Declare(files)
+	files = removeNonIncluded(files)
+	let count = 0 // Resolve the dependency order.
+	for (let defPos = 0; defPos < files.length; defPos++) { 
+		if (count > (files.length * 100)) { // Obviously a hack.
+			throw new Error('Circular dependencies.')
+		}
+		const definition = files[defPos]
+		const include = 'include.' + getName(definition)
+		if (files.some((file, includePos) => defPos > includePos && file.content.includes(include))) {
+			files.splice(defPos, 1)
+			files.splice(defPos - 1, 0, definition)
+			defPos = 0 // Start over.
+			count++
+			continue
+		}
+	}
+	return files.map(wrap)
+}
+
+const api = /*js*/`
+const __deps = {};
+const include = new Proxy(__deps,{
+	set: function(_,prop){throw new Error('Attempted to set property "' + prop  + '" on include.')},
+	get: function(target,prop){
+		if(prop in target){return target[prop]}
+		else{throw new Error(prop + ' has not been declared.')}
+	}
+});
+const declare = new Proxy(__deps,{
+	get: function (_,prop) {throw new Error('Attempted to read property "' + prop + '" from declare.')},
+	set: function (obj,prop,v) {
+		if(prop in obj){throw new Error(prop + ' has already been declared.')}
+		else{obj[prop]=v;return true}
+	}
+});
+`
+
+function compile(dirPath) {
+	const files = readdir(dirPath)
+	const code = organize(files)
+	const setup = api.split('\n').join('')
+	return (
+		''.padStart(40, '\t') + 
+		'/*__COMP*/void function(){\'use strict\';' + setup + code.join('') + 
+		'\n' + ''.padStart(40, '\t') + '}()'
+	)
+}
+
+const cwd = process.cwd()
+writeFileSync(join(cwd, '..', basename(cwd) + '.compiled.js'), compile(cwd))
