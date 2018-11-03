1c18de2c5b617a7b83ef7888867a61d8a398d0eb
Jeff Mcmahan <jeff@Jeffs-MacBook-Pro.local>
2018-11-03 12:28:29 -0400

    comment-strip bugfix

--- utils/strip-comments.js
+++ utils/strip-comments.js
@@ -4,6 +4,27 @@ module.exports = function (src) {
 	let pos = 0
 	const ln = src.length
 	while (pos < ln) {
+
+		const notEscaped = src[pos-1] !== '\\'
+		const next = src[pos]
+
+		// String literals
+		if (next === '"' && notEscaped) {
+			pos++
+			pos += src.slice(pos).indexOf('"') + 1
+			continue
+		}
+		if (next === '\'' && notEscaped) {
+			pos++
+			pos += src.slice(pos).indexOf('\'') + 1
+			continue
+		}
+		if (next === '`' && notEscaped) {
+			pos++
+			pos += src.slice(pos).indexOf('`') + 1
+			continue
+		}
+
 		const next2 = src.slice(pos, pos + 2)
 		if (next2 === '//') {
 			const commentLn = src.slice(pos).indexOf('\n')
